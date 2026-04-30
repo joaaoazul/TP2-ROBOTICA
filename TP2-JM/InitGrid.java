a43e37a3b64a4296b63bef6470dbf41ee7d4a979
Martim Aquino <martimaquino01@gmail.com>
2026-04-30 23:50:08 +0100

    Update InitGrid.java

--- TP2-JM/InitGrid.java
+++ TP2-JM/InitGrid.java
@@ -3,37 +3,104 @@ import java.util.Scanner;
 
 public class InitGrid {
 
-    
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
+
+        private int gridHeight = 0;
+        private int gridWidth = 0;
+        private String[][] grid;
+
         Scanner gridValues = new Scanner(System.in);
         String gridRawValues = gridValues.nextLine();
         String[] values = gridRawValues.trim().split(" ");
 
-        if (values.length == 2){ //validação para ver se tem 2 valores 
+        if (values.length == 2){ //atribuir valores a input e validação para ver se tem 2 valores 
             
             //primeiro vamos extrair os valores do nosso array e depois atribuir o valor de inteiro
             
             String rawWidthStr = values[0]; //largura
-            int gridWidth = Integer.parseInt(rawWidthStr);
+            gridWidth = Integer.parseInt(rawWidthStr);
 
             String rawHeightStr = values[1]; //altura
-            int gridHeight = Integer.parseInt(rawHeightStr);
+            gridHeight = Integer.parseInt(rawHeightStr);
 
         }else {
             System.out.println("Error: Your input must have 2 values");
         }
 
-        for (int i = 1; i < gridWidth + 1; i++) {
-            System.out.printf("%02d", i); //%02d para ficar 01 e nao 1
+        //validação da posição
+        private boolean isAvaliable(int l, int c) {
+            return l >= 0 && l < gridHeight && c >= 0 && c < gridWidth;
         }
-        for (int i = 1; i < gridHeight + 1; i++) {
-            System.out.printf("%02d", i);
+
+
+        //inicializar objeto da tabela
+        public Grid(int width, int height) {
+            this.gridWidth = width;
+            this.gridHeight = height;
+            this.grid = new String[height][width];
+            startGrid();
         }
+
+        //meter .. em todos os espaços e as numerações
+        private void startGrid() {
+
+            int y=0;
+            int o=0;
+
+
+            System.out.print("   ");
+            for (int i = 0; i < gridWidth; i++) { //imprimir a primeira linha com a numeração
+                o++;
+                System.out.printf("%02d",o);
+                System.out.print(" ");
+            }
+            System.out.println();
+            for (int l = 0; l < gridHeight; l++) { //imprimir a tabela em si
+                y++;
+                System.out.printf("%02d", y);
+                System.out.print(" ");
+                for (int c = 0; c < gridWidth; c++) {
+                    
+                    System.out.print(grid[l][c] + " ");
+                }
+                System.out.println();
+                
+            }
+                for (int l = 0; l < gridHeight; l++) { //preencher os espaços todos com ..
+                    for (int c = 0; c < gridWidth; c++) {
+                        grid[l][c] = "..";
+                    }
+                
+                }
+                
+        }
+
+        public void setGridEntity (int l, int c, String entity){
+            if (isAvaliable(l,c) == true) {
+                grid[l][c] = entity;
+                
+            }
+        }
+
+        public String getGridEntity (int l, int c) {
+            return grid[l][c];
+        }
+
+
+
+
+
+
         
+
+
+
+
+            
     }
 
-   // gridHeight = gridRawValues.charAt(2)
 
 
 }
+
