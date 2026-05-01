fb3013b864ec93f421805b58c5739a01573c1906
joaaoazul <joao.azul.11@outlook.com>
2026-05-01 16:48:29 +0100

    Add InputReader class for input handling and validation; implement constructor and methods for reading project and dimensions

--- TP2-JM/InputReader.java
+++ TP2-JM/InputReader.java
@@ -1,8 +1,39 @@
-
+import java.util.Scanner;
+import java.util.List;
 // ler os inputs e validar
 //Apenas validar inputs de range, se é number, etc
 //Não há validações de funcionalidade aqui
 
 public class InputReader {
-    
+
+    private Scanner scanner = new Scanner(System.in);
+
+    //Construtor
+    public InputReader(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    //métodos principais da classe
+
+    //readProject é o único público porque vai ser chamado de fora
+    public Project readProject(){
+
+    }
+
+    private int[] readDimensions(){
+
+    }
+
+    private void readEntities(){
+
+    }
+
+    //métodos de apoio
+    private boolean isValidCoordinates(int x, int y, int width, int height){
+
+    }
+
+    private boolean isPositionOccupied(int x, int y, List<Entity> entities){
+
+    }
 }
